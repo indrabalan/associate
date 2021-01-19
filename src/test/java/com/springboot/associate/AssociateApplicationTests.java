2a56782b94f2bffebf8b9593ae8cd2e59e5cc1f9
indrabalan <indran3055@gmail.com>
2021-01-19 13:03:43 +0530

    associate

---
+++ src/test/java/com/springboot/associate/AssociateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.associate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
